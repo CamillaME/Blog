c32edf719b531e6b5d1ce2d4330882cc675d8800
denra <denra82@gmail.com>
2017-09-28 11:10:08 +0200

    EntryController, views, models og View models
    Disse er bare blev lavet ikke programmet endnu.

---
+++ Blog/Models/EntryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class EntryModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ Blog/ViewModels/EntryVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.ViewModels
+{
+    public class EntryVM
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Text { get; set; }
+
+        public List<EntryVM> Entries { get; set; }
+    }
+}
