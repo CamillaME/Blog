95671fab6e93d1dcdc1ee8aa4e3f92d70074f153
denra <denra82@gmail.com>
2017-10-06 10:21:10 +0200

    Forside index
    Home Index siden, brugt noget af det samme kode fra entry index.

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog.DAL;
+using Blog.Models;
+using Blog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +13,34 @@ namespace Blog.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            EntryRepository entryRepository = new EntryRepository();
+
+            List<EntryModel> entries = entryRepository.GetAllEntries();
+
+            List<EntryVM> entriesVms = new List<EntryVM>();
+
+            foreach (EntryModel entry in entries)
+            {
+                if(entry.EntryIsPublished == true)
+                {
+                    EntryVM entryVM = new EntryVM();
+                    entryVM.Id = entry.EntryID;
+                    entryVM.Title = entry.EntryTitle;
+                    entryVM.Text = entry.EntryText;
+                    entryVM.Date = entry.EntryDate;
+                    entryVM.IsPublished = entry.EntryIsPublished;
+                    entriesVms.Add(entryVM);
+                }
+                else
+                {
+                    continue;
+                }
+                
+            }
+
+            EntryVM model = new EntryVM();
+            model.Entries = entriesVms;
+            return View(model);
         }
 
         public ActionResult About()
